49444890e4fc6638860a87d99458d52d19c7cb5c
elvikamalt <elvi_kamalt@mail.ru>
2021-10-31 03:20:49 +0500

    Исправлен вид Django admin для пользователей и категорий

--- users/admin.py
+++ users/admin.py
@@ -7,3 +7,6 @@ from baskets.admin import BasketAdmin
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     inlines = (BasketAdmin,)
+    list_display = ('username', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+    ordering = ('username',)
+    search_fields = ('username',)
