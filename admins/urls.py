bd74f5c35d0249fc4c1105baf89a003eb449d334
elvikamalt <elvi_kamalt@mail.ru>
2021-10-31 22:21:35 +0500

    Добавлен UserDeleteView

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from admins.views import index, UserCreateView, UserListView, UserUpdateView, admin_users_delete, \
+from admins.views import index, UserCreateView, UserListView, UserUpdateView, UserDeleteView, \
     admin_products_create, admin_products, admin_products_update, admin_products_delete, \
     admin_categories_create, admin_categories, admin_categories_update, admin_categories_delete
 
@@ -11,7 +11,7 @@ urlpatterns = [
     path('users/', UserListView.as_view(), name='admin_users'),
     path('users-create/', UserCreateView.as_view(), name='admin_users_create'),
     path('users-update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
-    path('users-delete/<int:id>/', admin_users_delete, name='admin_users_delete'),
+    path('users-delete/<int:pk>/', UserDeleteView.as_view(), name='admin_users_delete'),
     path('products/', admin_products, name='admin_products'),
     path('products-create/', admin_products_create, name='admin_products_create'),
     path('products-update/<int:id>/', admin_products_update, name='admin_products_update'),
--- admins/views.py
+++ admins/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib import messages
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from users.models import User
 from products.models import Product, ProductCategory
@@ -34,6 +34,12 @@ class UserUpdateView(UpdateView):
     form_class = UserAdminProfileForm
     success_url = reverse_lazy('admins:admin_users')
     template_name = 'admins/admin-users-update-delete.html'
+
+
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admins:admin_users')
 
 
 # Create
@@ -83,11 +89,11 @@ class UserUpdateView(UpdateView):
 
 
 # Delete
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_delete(request, id):
-    user = User.objects.get(id=id)
-    user.safe_delete()
-    return HttpResponseRedirect(reverse('admins:admin_users'))
+# @user_passes_test(lambda u: u.is_staff)
+# def admin_users_delete(request, id):
+#     user = User.objects.get(id=id)
+#     user.safe_delete()
+#     return HttpResponseRedirect(reverse('admins:admin_users'))
 
 
 # Create product
