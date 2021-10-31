6abb91fe46b59597897a354d854faf79f554322c
elvikamalt <elvi_kamalt@mail.ru>
2021-10-31 22:26:14 +0500

    Перегружен метод удаления пользователя

--- admins/views.py
+++ admins/views.py
@@ -41,6 +41,12 @@ class UserDeleteView(DeleteView):
     template_name = 'admins/admin-users-update-delete.html'
     success_url = reverse_lazy('admins:admin_users')
 
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        self.object.safe_delete()
+        return HttpResponseRedirect(success_url)
+
 
 # Create
 # @user_passes_test(lambda u: u.is_staff)
